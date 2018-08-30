a81d7141ee2f67524ca43e8901f27db361419ab0
AkshayUChidrawar <30985227+AkshayUChidrawar@users.noreply.github.com>
2018-08-30 23:11:20 +0530

    Add files via upload

---
+++ week1/assignments/problem1_3.py
@@ -0,0 +1,7 @@
+def problem1_3(n):
+    my_sum = 0
+    i = 1
+    while i < n+1:
+        my_sum = my_sum + i
+        i = i + 1
+    print (my_sum)
---
+++ week1/assignments/problem1_4.py
@@ -0,0 +1,3 @@
+def problem1_4(miles):
+    f = 5280 * miles
+    print ("There are", f,"feet in", miles,"miles.")
